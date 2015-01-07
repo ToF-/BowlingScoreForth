135cbb4143c638fc00c262056badc58191abe1e5
ToF- <cthibaut@octo.com>
2015-01-07 23:02:55 +0100

    first bonus calc

--- Bowling.fs
+++ Bowling.fs
@@ -1,7 +1,25 @@
 ( Bowling.fs )
 
-: dummy-test
-    assert( true ) ;
+0 constant normal
+2 constant strike
+ 
+: bonus ( r,f -- n )
+    if 
+        10 = if strike else normal then
+    else 
+        drop normal 
+    then ;
+        
 
-dummy-test
+: bonus_should_be_0_when_first_roll_and_not_strike
+    4 true 
+    bonus assert( 0= ) ;
+
+: bonus_should_be_2_when_first_roll_and_strike
+    10 true
+    bonus assert( 2 = ) ;
+
+bonus_should_be_0_when_first_roll_and_not_strike
+bonus_should_be_2_when_first_roll_and_strike
+
 bye
