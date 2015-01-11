fa2b3b44eb4b1160418d95ed94585fca665d7eaf
ToF- <cthibaut@octo.com>
2015-01-11 18:44:21 +0100

    encoding status info

--- Bowling.fs
+++ Bowling.fs
@@ -9,17 +9,41 @@
 1 constant spare
 2 constant strike
 
-create statuses  ( usual0   bonus1   boni11   boni21 )
+create bonuses   ( usual0   bonus1   boni11   boni21 )
      ( normal )    usual0 , usual0 , bonus1 , bonus1 ,
      ( spare  )    bonus1 , bonus1 , bonus1 , bonus1 ,
      ( strike )    boni11 , boni11 , boni21 , boni21 ,
 
 : quality-offset ( quality -- line-offset )
     4 cells * ;
-: status-offset  ( status  -- col-offset )
+: status-offset  ( bonus  -- col-offset )
     cells ;
 
-: new-status ( status,quality -- status )
+: new-bonus ( bonus,quality -- bonus )
     quality-offset swap 
-    status-offset + statuses + @ ;
+    status-offset + bonuses + @ ;
+
+: get-bonus ( status -- bonus ) 
+    5 rshift 3 and ;
+
+: set-bonus ( bonus,status -- status )
+    swap 5 lshift or ;
+
+: get-frame ( status -- frame )
+    15 and ;
+
+: set-frame ( frame,status -- status )
+    or ;
+
+: in-frame? ( status -- flag )
+    16 and ;
+
+: end-frame ( status -- status )
+    239 and ;
+
+: in-frame ( status -- status )
+    16 or ;
+
+
+
     
--- Tests.fs
+++ Tests.fs
@@ -2,11 +2,24 @@ s" Bowling.fs" included
 
 : tests
 
-( new-status should determine new status from previous status and roll quality )
-    usual0 normal assert( new-status usual0 = ) 
-    boni11 strike assert( new-status boni21 = )
-    boni21 strike assert( new-status boni21 = )
-    bonus1 normal assert( new-status usual0 = )
+( new-bonus should determine new status from previous status and roll quality )
+    usual0 normal assert( new-bonus usual0 = ) 
+    boni11 strike assert( new-bonus boni21 = )
+    boni21 strike assert( new-bonus boni21 = )
+    bonus1 normal assert( new-bonus usual0 = )
+
+( set-bonus and get-bonus should store and retrieve bonus info )
+    boni21 0 set-bonus assert( get-bonus boni21 = )
+
+( set-frame and get-frame should store and retrieve frame number )
+    7 0 set-frame assert( get-frame 7 = )
+
+( in-frame? end-frame and in-frame should keep in-frame status )
+    7 0 set-frame boni21 swap set-bonus
+    dup in-frame  assert( in-frame? ) 
+    end-frame assert( in-frame? 0 = ) 
+    
+
 ;
 tests .s
 
