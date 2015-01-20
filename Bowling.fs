2fb61c410dbd479d19e893e3a018989c3609c3bb
ToF- <cthibaut@octo.com>
2015-01-20 23:44:05 +0100

    adjusting frames

--- Bowling.fs
+++ Bowling.fs
@@ -10,25 +10,21 @@ variable frame
 variable bonus
 variable last-roll
 
-: in-game ( -- 1|0 )
-    frame @ 10 < 1 and ;
+: in-game ( -- 1|0 )   frame @ 10 < 1 and ;
 
-: c-bonus ( -- 0|1|2 )
-    bonus @ 3 and ;
+: current-bonus ( -- 0|1|2 ) bonus @ 3 and ;
 
-: factor ( roll -- n )
-    c-bonus in-game + * ;
+: next-bonus ( -- 0|1 )   bonus @ 2 rshift ;
 
-: end-frame
-    new-frame last-roll ! ;
+: factor ( roll -- n ) current-bonus in-game + * ;
+
+: end-frame new-frame last-roll ! ;
    
-: all-down 
-    10 = ;
+: all-down 10 = ;
  
 : 1st-roll-type ( roll -- t )
     dup all-down if drop strike end-frame 
     else last-roll ! first then ;
-
 
 : 2nd-roll-type ( roll,last -- t )
     + all-down if spare else second then
@@ -37,5 +33,22 @@ variable last-roll
 : roll-type ( roll -- t )
     last-roll @ dup new-frame and 
     if drop 1st-roll-type else 2nd-roll-type then ;
+
+: spare! ( b -- b )  1 or ;
+
+: strike! ( b -- b ) 5 + ;
+
+: roll-bonus ( type,b -- b )
+    swap dup spare = if  drop spare!
+    else    strike = if strike!
+    then then  ;
+    
+: new-bonus ( type -- b )
+    next-bonus 
+    in-game if roll-bonus else swap drop then ; 
+
+: adjust-frame 
+    in-game last-roll @ new-frame = and if 1 frame +! then ;
+     
 
 
--- Tests.fs
+++ Tests.fs
@@ -12,8 +12,9 @@ s" Bowling.fs" included
 0  frame !  in-game assert( 1 equals )
 10 frame !  in-game assert( 0 equals )
 
-5 bonus !   c-bonus assert( 1 equals )
-6 bonus !   c-bonus assert( 2 equals )
+5 bonus !   current-bonus assert( 1 equals )
+6 bonus !   current-bonus assert( 2 equals )
+6 bonus !   next-bonus assert( 1 equals )
 
 5 bonus !  3 frame !  5 factor assert( 10 equals )
 6 bonus !  3 frame !  5 factor assert( 15 equals )
@@ -26,6 +27,18 @@ end-frame   4 roll-type drop last-roll @ assert( 4 equals )
 4 last-roll ! 6  roll-type assert( spare equals ) 
 4 last-roll ! 6  roll-type drop last-roll @ assert( new-frame equals )
 
+0 bonus ! spare new-bonus assert( 1 equals )
+5 bonus ! strike new-bonus assert( 6 equals )
+10 frame !
+0 bonus ! spare new-bonus assert( 0 equals )
+5 bonus ! strike new-bonus assert( 1 equals )
+
+0 frame !
+4 last-roll !  adjust-frame  frame @ assert( 0 equals )
+end-frame  adjust-frame frame @ assert( 1 equals )
+
+10 frame !
+end-frame  adjust-frame frame @ assert( 10 equals )
 ; 
 tests
 ~~
