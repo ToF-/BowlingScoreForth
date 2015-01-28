e0cc16472e3cc558eefdef91364b4bda9e46bd2b
ToF- <cthibaut@octo.com>
2015-01-28 12:05:14 +0100

    keep track of last roll
    with clear set get fields strategy

--- Bowling.fs
+++ Bowling.fs
@@ -1,10 +1,40 @@
 \ Bowling.fs
 
-: start-game        ( -- game )
-    0 ;
+: clear             ( w -- w )
+    -1 xor and ;
 
-: score             ( game -- score )
-    ;
+: get               ( word,size,pos -- word )
+    rot swap rshift swap and ;
+
+: set               ( word,value,size,pos -- word )
+    rot over lshift >r 
+    lshift clear r> or ;
+
+: score       ( -- size,pos )
+    4095 0 ;
+
+: track       ( -- size,pos )
+    15  12 ;
+
+: score+            ( game,roll -- game )
+    over score get + score set ;
+
+: track-2nd-roll    ( roll -- track )
+    drop 11 ;
+
+: track-1st-roll    ( roll -- track )
+    dup 10 = if track-2nd-roll then ;
+
+: track!            ( game,roll -- game )
+    over track get  
+    11 = if track-1st-roll 
+    else    track-2nd-roll then
+    track set ;  
+
+: start-game        ( -- game )
+    0 11 track set ;
 
 : add-roll          ( game,roll -- game )
-     + ;
+    swap over
+    score+ 
+    swap track! ;
--- Tests.fs
+++ Tests.fs
@@ -4,12 +4,20 @@ s" Bowling.fs" included
 
 : tests
 cr ." initial score should be zero "         
-start-game score assert( 0= ) 
+start-game score get assert( 0= ) 
 
 cr ." adding a roll should increase score "
-start-game 7 add-roll score assert( 7 = )
+start-game 7 add-roll dup score get assert( 7 = )
+           2 add-roll     score get assert( 9 = )
 
+cr ." adding a roll should leave a track "
+start-game  dup  track get assert( 11 = ) 
+ 7 add-roll dup  track get assert(  7 = )
+ 2 add-roll dup  track get assert( 11 = )
+10 add-roll dup  track get assert( 11 = )
+ 5 add-roll      track get assert(  5 = )
 ;
 
+hex
 tests
 cr .s
