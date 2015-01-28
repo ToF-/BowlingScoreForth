f0ade9c373b6c406abac27047da4978214b7aab8
ToF- <cthibaut@octo.com>
2015-01-28 16:43:29 +0100

    best version so far

--- Bowling.fs
+++ Bowling.fs
@@ -12,8 +12,17 @@
 
 : score 4095 0 ; ( -- size,pos )
 : track 15  12 ; ( -- size,pos )
+: bonus  7  16 ; ( -- size,pos )
+: frame 15  19 ; ( -- size,pos )
+
+: bonus-factor      ( game -- factor )
+    bonus get 3 and ;
+
+: frame-factor      ( game -- factor )
+    frame get 10 / 1 swap - ;
 
 : score+            ( game,roll -- game )
+    over dup bonus-factor swap frame-factor + * 
     over score get + score set ;
 
 : close-frame    ( roll -- track )
@@ -28,10 +37,46 @@
     else    close-frame then
     track set ;  
 
+0 constant first 
+1 constant second
+2 constant spare
+3 constant strike
+
+: roll-type         ( roll,game -- type )
+    track get 
+    + dup 21 = if drop strike else 
+      dup 10 = if drop spare  else
+          10 < if second else
+                  first 
+    then then then ; 
+
+: next-bonus        ( game -- bonus )
+    bonus get 2 rshift ; 
+
+: new-bonus         ( bonus,type -- bonus )
+    dup strike = if drop 5 +  else
+        spare  = if      1 or then then ;
+
+: bonus!            ( game,roll -- game )
+    over next-bonus -rot
+    over roll-type 
+    over frame-factor * 
+    rot swap
+    new-bonus bonus set ;
+
+: frame!            ( game -- game )
+    dup  frame get
+    over track get
+    11 = if 1+ 10 min then 
+    frame set ;
+  
 : start-game        ( -- game )
     0 11 track set ;
 
 : add-roll          ( game,roll -- game )
-    swap over
-    score+ 
-    swap track! ;
+    swap over       ( roll,game,roll )
+    score+          ( roll,game )
+    over            ( roll,game,roll )
+    bonus!          ( roll,game )
+    swap track!     ( game )
+    frame! ;        ( game )
--- Tests.fs
+++ Tests.fs
@@ -16,7 +16,37 @@ start-game  dup  track get assert( 11 = )
  2 add-roll dup  track get assert( 11 = )
 10 add-roll dup  track get assert( 11 = )
  5 add-roll      track get assert(  5 = )
+
+cr ." a spare should generate bonus "
+start-game  4 add-roll 6 add-roll bonus get assert( 1 = )
+
+cr ." a strike should generate bonus "
+start-game 10 add-roll bonus get assert( 5 = )
+
+cr ." after a strike a bonus should remain "
+start-game 10 add-roll 4 add-roll bonus get assert( 1 = )
+
+cr ." two strikes should generate bonus "
+start-game 10 add-roll 10 add-roll bonus get assert( 6 = )
+
+cr ." score takes existing bonus into account "
+start-game 10 add-roll 5 add-roll score get assert( 20 = )
+
+cr ." adding rolls increase frame "
+start-game 3 add-roll dup frame get assert( 0 = )
+           3 add-roll dup frame get assert( 1 = )  
+          10 add-roll     frame get assert( 2 = ) 
+
+cr ." after 10 frames roll don't add to score " 
+start-game 10 frame set 5 add-roll score get assert( 0 = ) 
+
+cr ." after 10 frame strikes don't make bonus "
+start-game 10 frame set 10 add-roll bonus get assert( 0 = ) 
+
+cr ." a perfect game scores 300 "
+start-game 12 0 do 10 add-roll loop score get assert( 300 = )
 ;
+
 
 hex
 tests
