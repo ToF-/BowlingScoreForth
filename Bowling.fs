c6764f33937b283056a549adc13ab45a3e78e8f6
ToF- <cthibaut@octo.com>
2015-01-28 12:15:21 +0100

    refactoring

--- Bowling.fs
+++ Bowling.fs
@@ -3,7 +3,7 @@
 : clear             ( w -- w )
     -1 xor and ;
 
-: get               ( word,size,pos -- word )
+: get               ( word,size,pos -- field )
     rot swap rshift swap and ;
 
 : set               ( word,value,size,pos -- word )
@@ -19,16 +19,16 @@
 : score+            ( game,roll -- game )
     over score get + score set ;
 
-: track-2nd-roll    ( roll -- track )
+: close-frame    ( roll -- track )
     drop 11 ;
 
 : track-1st-roll    ( roll -- track )
-    dup 10 = if track-2nd-roll then ;
+    dup 10 = if close-frame then ;
 
 : track!            ( game,roll -- game )
     over track get  
     11 = if track-1st-roll 
-    else    track-2nd-roll then
+    else    close-frame then
     track set ;  
 
 : start-game        ( -- game )
